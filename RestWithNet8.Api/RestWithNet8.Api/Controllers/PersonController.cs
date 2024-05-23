d59ed196577942dc8569484e582abf3e7c561e6a
SEGURADORALIDER\luizmarinho.brq <luizmarinho.brq@seguradoralider.com.br>
2024-05-23 20:19:23 -0300

    Adicionando model Person

--- RestWithNet8.Api/RestWithNet8.Api/Controllers/PersonController.cs
+++ RestWithNet8.Api/RestWithNet8.Api/Controllers/PersonController.cs
@@ -4,16 +4,16 @@ namespace RestWithNet8.Api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class PersonController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<PersonController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public PersonController(ILogger<PersonController> logger)
         {
             _logger = logger;
         }
